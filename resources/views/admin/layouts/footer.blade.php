69b4054324f7f1f9a565148e16b84cc2e08d870d
ShakilAhmed007 <sakilhossain01969@gmail.com>
2021-09-27 01:20:09 +0600

    ➕ Add: footer updated

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 
 <footer class="main-footer">
   <div class="footer-left">
-    Copyright &copy; 2019 <div class="bullet"></div> Design By <a href="#">Redstar</a>
+    Copyright &copy; 2021 <div class="bullet"></div> Design Develop By <a target="_blank" href="https://bditfactory.com">It Factory</a>
   </div>
   <div class="footer-right">
   </div>
