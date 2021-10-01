92812e0947fb8813b249c3998e98c1e82bffe8d8
iammashikur <700mirash@gmail.com>
2021-10-01 12:43:21 +0600

    :construction: Working: Working on checkout

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Book;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Generator as Faker;
 
 class BookFactory extends Factory
 {
@@ -22,17 +23,17 @@ class BookFactory extends Factory
     public function definition()
     {
         return [
-            'user_id'     => 4,
+            'user_id'     => 1,
             'library_id'  => 1,
             'category_id' => 1,
-            'cover_image' => 'https://covers.zlibcdn2.com/covers/books/f3/1b/5a/f31b5aa50e3b8a101c257085d0ed3ce2.jpg',
-            'title'       => 'Ikigai: The Japanese Secret to a Long and Happy Life',
+            'cover_image' => $this->faker->image('public/uploads/images',640,480, null, false),
+            'title'       => $this->faker->word,
             'slug'        => 'Ikigai-The-Japanese-Secret-to-a-Long-and-HappyLife',
             'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, sint excepturi ipsam architecto atque nemo sequi quia, in, eligendi maiores molestiae unde alias! Temporibus maiores et quia dignissimos qui exercitationem.',
             'price'       => '500',
             'stock'       => '100',
-            'writer'      => 'Héctor García, Francesc Miralles',
-            'publisher'   => 'Francesc Miralles',
+            'writer'      => $this->faker->name,
+            'publisher'   => $this->faker->name,
             'tags'        => 'Ikigai: The Japanese Secret to a Long and Happy Life',
             'status'      => 1
         ];
